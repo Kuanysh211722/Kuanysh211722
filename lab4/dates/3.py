b387f4dff99e8fea54c216fe4d4b7d975be7a62b
Kuanysh211722 <99401986+Kuanysh211722@users.noreply.github.com>
2022-03-04 21:45:09 +0600

    Add files via upload

---
+++ lab4/dates/3.py
@@ -0,0 +1,9 @@
+import datetime
+dt = datetime.datetime.today().replace(microsecond=0)
+print(dt)
+
+
+
+
+
+
---
+++ lab4/dates/4.py
@@ -0,0 +1,9 @@
+from datetime import datetime, time
+def date_diff_in_Seconds(dt2, dt1):
+  timedelta = dt2 - dt1
+  return timedelta.days * 24 * 3600 + timedelta.seconds
+
+date1 = datetime.strptime('2022-03-04 22:00:00', '%Y-%m-%d %H:%M:%S')
+
+date2 = datetime.now()
+print("%d seconds" %(date_diff_in_Seconds(date2, date1)))
